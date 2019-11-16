5861e3ddec8a0c4e51d69efa36cedd9b68aa40ef
Jeevan Joshi <jeevan.z.joshi@gmail.com>
2019-11-16 21:37:58 +0530

    Commit Capstone-Project-Notebook

---
+++ Capstone-Project-Notebook.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Peer-graded Assignment: Capstone Project Notebook
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
